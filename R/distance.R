86845959469b690683ddc0ba8ef1e7ab55f1d3fa
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2022-05-19 22:11:01 -0400

    fixed lints

--- R/distance.R
+++ R/distance.R
@@ -1,10 +1,11 @@
 #' @title Euclidean distance
-#' @description Calculates Euclidean distance between two vectors. An error will be 
-#' given if NAs are present in either vector.
+#' @description Calculates Euclidean distance between two vectors. An error
+#' will be given if NAs are present in either vector.
 #'
 #' @param a The first vector to use in the distance calculation.
 #' @param b The second vector to use in the distance calculation.
-#' @param verbose Boolean. If \code{TRUE}, a message will be printed. Default \code{TRUE}.
+#' @param verbose Boolean. If \code{TRUE}, a message will be printed.
+#' Default \code{TRUE}.
 #' @return A numeric value of a distance
 #' @examples
 #' data(example_data)
@@ -14,23 +15,23 @@ euclideanDist <- function(a, b, verbose = FALSE) {
   if (isTRUE(verbose)) {
     message("Calculating distance ...")
   }
-  
+
   # Check validity of data
-  .check_data(a)
-  .check_data(b)
-  
+  .checkData(a)
+  .checkData(b)
+
   # Perform calculation
-  res <- sqrt(sum((a-b)^2))
+  res <- sqrt(sum((a - b) ^ 2))
   return(res)
 }
 
 #' Validity check
-#' 
+#'
 #' This functions checks for NAs in a vector
 #'
 #' @param input A numeric vector
 #' @return Nothing returned
-.check_data <- function(input) {
+.checkData <- function(input) {
   if (any(is.na(input))) {
     stop("'input' must not contain NAs")
   }
