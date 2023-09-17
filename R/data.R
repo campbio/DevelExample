d024eba85da7dd7466f989ab4997d8b388d57670
Nathan Palamuttam <npalamuttam04@gmail.com>
2023-09-17 19:27:49 -0400

    add docs

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Example dataset
 #'
 #' A dataset containing a matrix with two columns that were generated
-#' with a random normal distribution with a mean of 0 and stdev of 1.
+#' with a random normal distribution with a mean of 0 and stdev of 1. 
 #'
 #' @format A matrix with 100 rows and 2 columns
 #' @keywords datasets
